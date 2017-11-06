669598f010a7bbcfc669a9b7740077c011cef102
Gary Li <garyli14916@starlite.me>
2017-11-05 17:49:36 -0800

    tab fuctionality

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+function openTab(event, tabName) {
+	var i, tabContent, tabLinks;
+	
+	//hide tabs
+	tabContent = $('.tab_content');
+	for (i = 0 ; i < tabContent.length ; i ++) {
+		tabContent[i].style.display = "none";
+	}
+	
+	//deactivate tabs
+	tabLinks = $('.tab_links');
+	for (i = 0 ; i < tabLinks.length ; i ++) {
+		tabLinks[i].className = tabLinks[i].className.replace("active", ""); 
+	}
+	
+	//show current tab
+	$("#" + tabName)[0].style.display = "block";
+	event.currentTarget.className += "active";
+}
