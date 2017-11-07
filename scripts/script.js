3f98533e781e9dde9285a7d3d4ab9dfd0068deb3
Gary Li <garyli14916@starlite.me>
2017-11-07 06:42:26 -0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -148,7 +148,7 @@ function selectDate(arr, d) {
 	for (var i = 0 ; i < arr.length; i ++) {
 		if (arr[i]["time"] <= new Date(d.getFullYear(), d.getMonth(), d.getDate(), 23, 59, 59)
         	&& arr[i]["time"] >= new Date(d.getFullYear(), d.getMonth(), d.getDate())) {
-			result.push(arr);
+			result.push(arr[i]);
 		}
 	}
 	return result;
